7ff6bce09d95e1c47c31a46a272f01d177ccfdc0
Henry <henry@localhost.localdomain>
2015-08-09 22:30:44 -0400

    Added clone from github functionality, added vimrc and bashrc to repo

---
+++ .vimrc
@@ -0,0 +1,22 @@
+colorscheme desert
+syntax enable
+set tabstop=4
+set expandtab
+set number
+set showmatch
+set foldenable
+set foldlevelstart=10
+set foldnestmax=10
+nnoremap <space> za
+set foldmethod=indent
+
+execute pathogen#infect()
+
+set statusline+=%#warningmsg#
+set statusline+=%{SyntasticStatuslineFlag()}
+set statusline+=%*
+
+let g:syntastic_always_populate_loc_list = 1
+let g:syntastic_auto_loc_list = 1
+let g:syntastic_check_on_open = 1
+let g:syntastic_check_on_wq = 1
--- conf.bash
+++ conf.bash
@@ -1,17 +1,42 @@
 #!/usr/bin/env bash
-printf "This script is for remotely cloning the configuration of any machine which is online and is capable of connecting with scp."
 
 if [[ $UID -ne 0 ]]; then
   printf "This script must be executed with root privileges. If you are not root, execute this script with the command sudo ./conf.bash instead of just ./conf.bash."
 fi
 
-printf "Which ip address would you like to clone from? IP:"
-read ipaddress
+chooseremote() {
+printf "Clone from (R)emote or (G)ithub?"
+while read -r -n 1 -s answer; do:
+  if [[ $answer = [RrGg] ]]; then
+    [[ $answer = [Rr] ]] && retval=0
+    [[ $answer = [Gg] ]] && retval=1
+    break
+  fi
+done
 
-printf "Which user on %s would you like to clone? User:" "$ipaddress"
-read user
+if chooseremote; then
+  printf "This option is for remotely cloning the configuration of any machine which is online and is capable of connecting with scp."
 
-scp .git-prompt.sh .bashrc .vimrc "$user"@"$ipaddress":~/
+  printf "Which ip address would you like to clone from? IP:"
+  read ipaddress
+
+  printf "Which user on %s would you like to clone? User:" "$ipaddress"
+  read user
+
+  scp .git-prompt.sh .bashrc .vimrc "$user"@"$ipaddress":~/
+else
+  printf "Cloning configuration from github"
+  wget https://github.com/151henry151/workspace-clone-utility/blob/master/.bashrc -O ~/.bashrc
+  wget https://github.com/151henry151/workspace-clone-utility/blob/master/.vimrc -O ~/.vimrc
+  wget https://github.com/151henry151/workspace-clone-utility/blob/master/.git-prompt.sh -O ~/.git-prompt.sh
+fi
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cd ~/.vim/bundle && \
+git clone https://github.com/scrooloose/syntastic.git  
 
 printf "Perform an update, an upgrade, and install python-virtualenv?"
 
