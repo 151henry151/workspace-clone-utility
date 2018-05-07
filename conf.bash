31b35a1537313a5f6d8e441197babf27976b9cab
Henry Romp <151henry151@gmail.com>
2018-05-07 14:57:32 -0400

    Merge gekko-install.bash into conf.bash

--- conf.bash
+++ conf.bash
@@ -102,9 +102,29 @@ fi
 printf "Install gekko and set up a gekko environment?"
 echo
 if asksure; then
-  wget "https://raw.githubusercontent.com/151henry151/workspace-clone-utility/master/gekko-install.bash" -O "/home/"$localuser"/gekko-install.bash"
-  chmod u+x "/home/"$localuser"/gekko-install.bash"
-  "/home/"$localuser"/gekko-install.bash"
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+apt install curl g++ gcc git nodejs build-essential -y
+cd /$localuser/
+git clone https://github.com/askmike/gekko.git
+cd /$localuser/gekko
+npm install --only=production
+npm install talib tulip tulind
+git clone https://github.com/gekkowarez/gekkoga.git
+cd gekkoga
+npm install
+cd /$localuser/gekko
+git clone https://github.com/tommiehansen/gekko_tools.git
+cp /$localuser/gekko/gekko_tools/strategies/*.js /$localuser/gekko/strategies/
+cp /$localuser/gekko/gekko_tools/strategies/*.toml /$localuser/gekko/config/strategies/
+git clone https://github.com/Gab0/gekko-extra-indicators.git
+cd /$localuser/gekko/gekko-extra-indicators/indicators
+cp *.js /$localuser/gekko/strategies/indicators
+cd /$localuser/gekko
+sed -i 's/127.0.0.1/0.0.0.0/g' /$localuser/gekko/web/vue/UIconfig.js
+sed -i 's/localhost/'${ipaddress}'/g' /$localuser/gekko/web/vue/UIconfig.js
+echo
+printf "Gekko and GekkoGA installed and ready for use. Edit /gekko/web/vue/UIconfig to set headless mode if desired."
+echo
 fi
 
 echo
